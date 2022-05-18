6729cb1f358ca3d183e74837cd6f058ed8c69340
mjaumi <mjaumi2864@gmail.com>
2022-05-19 04:42:56 +0600

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='mt-20'>
             <p className='text-info text-lg mb-2'>©Milhan Joardar Aumi, 2022.</p>
         </div>
     );
